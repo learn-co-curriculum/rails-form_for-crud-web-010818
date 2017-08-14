428ed7f0dc380e7802606add230c78f799146c85
alexgriff <alexalexgriffith@gmail.com>
2017-08-14 10:02:36 -0400

    Add starter problem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  helper_method :make_header, :make_link
+
+  def make_header(title)
+    "<h1>#{title}</h1>".html_safe
+  end
+
+  def make_link(text, path)
+
+  end
+
 end
